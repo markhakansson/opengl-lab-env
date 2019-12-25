2a30bc9bf29270297f72e108f05c073e0c6ac01c
Mark Hakansson <mark.hakansson@live.se>
2019-12-25 13:21:11 +0100

    Binary nodes f*cking finally works as intended

--- projects/example/code/tools.h
+++ projects/example/code/tools.h
@@ -8,9 +8,32 @@
  * 	If the points makes a clockwise turn the result will be negative.
  * 	If counter-clockwise the result is positive. If zero they are collinear.
  */
-inline float cross(glm::vec3 p0, glm::vec3 p1, glm::vec3 p2)
+inline float cross(glm::vec3 &p0, glm::vec3 &p1, glm::vec3 &p2)
 {
 	return (p0.x - p2.x) * (p1.y - p2.y) - (p0.y - p2.y) * (p1.x - p2.x);
+}
+
+/**
+ *  Check if point p is within the sector bounded by c, ci and cm.
+ */
+inline bool pointInSector(glm::vec3 c, glm::vec3 ci, glm::vec3 cm, glm::vec3 p)
+{
+	if (cross(c, cm, p) < 0 && cross(c, ci, p) > 0)
+	{
+		return true;
+	}
+	else if (cross(c, cm, p) > 0 && cross(c, ci, p) < 0)
+	{
+		return true;
+	}
+	else if (cross(c, cm, p) < 0 && cross(c, ci, p) < 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 #endif
